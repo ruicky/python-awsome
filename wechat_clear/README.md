4ba4722e3811159ada742934d6eb68a3c70c9ab4
ruirui.wang <ruirui.wang@fengjr.com>
2018-09-07 11:26:21 +0800

    😈Add

---
+++ wechat_clear/README.md
@@ -0,0 +1,13 @@
+# 清理微信僵尸用户
+
+> 实践日期：2018-09-07 11:06:55
+
+
+## 使用方法
+```
+$ pip install wxpy
+$ python wechat_clear.py
+```
+
+## 效果
+![demo](demo.png)
---
+++ wechat_clear/wechat_clear.py
@@ -0,0 +1,28 @@
+#encoding=utf-8
+from wxpy import *
+import time
+import random
+
+bot = Bot()
+
+all_friends = bot.friends()
+myself = bot.self
+
+print('----------------BEGIN----------------')
+
+print("检测到你联系人共计: "+ str(len(all_friends)) + " 人")
+
+index = 1;
+for user in all_friends:
+    time.sleep(random.randint(0,9))
+    try:
+        if user != myself:
+            print("["+str(index)+"/"+str(len(all_friends))+"] "+user.name)
+            user.send(' ॣ ॣ ॣ')
+    except ResponseError as e:
+        print(e.err_code, e.err_msg) 
+    index += 1    
+
+print("检测已执行完毕请到手机微信app中处理")
+
+print('----------------END----------------')   
